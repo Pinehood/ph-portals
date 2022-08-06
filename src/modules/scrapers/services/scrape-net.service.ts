3bed548a7834baf24dcced593073ccf3c57d9c9a
Baxy123 <44103797+Baxy123@users.noreply.github.com>
2022-08-06 10:03:46 +0200

    Update scrape-net.service.ts

--- src/modules/scrapers/services/scrape-net.service.ts
+++ src/modules/scrapers/services/scrape-net.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectPinoLogger, PinoLogger } from "nestjs-pino";
+import axios from "axios";
 import { Portals } from "@resources/common/constants";
 import { Article } from "@resources/dtos";
 import {
@@ -9,6 +10,7 @@ import {
   shouldArticleBeDisplayed,
 } from "@resources/common/functions";
 import { ScraperService } from "@scrapers/services/scraper.service";
+import * as cheerio from "cheerio";
 
 @Injectable()
 export class ScrapeNetService implements ScraperService {
@@ -26,11 +28,56 @@ export class ScrapeNetService implements ScraperService {
     this.name = getPortalName(this.type);
     this.link = "https://www.Net.hr";
     this.default = getDefaultArticle(this.type, this.link, this.name);
-    this.roots = []; //RSS
+    this.roots = [
+      "https://net.hr/feed/danas",
+      "https://net.hr/feed/sport",
+      "https://net.hr/feed/hot",
+      "https://net.hr/feed/magazin",
+      "https://net.hr/feed/webcaffe",
+    ]; //RSS
   }
 
   async scrape(): Promise<Article[]> {
     let articles: Article[] = [];
+    for (let i = 0; i < this.roots.length; i++) {
+      const rootLink = this.roots[i];
+      try {
+        const rss = await axios.get(rootLink);
+        if (rss && rss.data) {
+          const articleLinks = (rss.data as string)
+            .match(/<link>(.*?)<\/link>/g)
+            .map((articleLink) =>
+              articleLink.replace("<link>", "").replace("</link>", "")
+            )
+            .filter((articleLink) => articleLink != "https://net.hr");
+          if (articleLinks) {
+            for (let j = 0; j < articleLinks.length; j++) {
+              const articleLink = articleLinks[j];
+              if (
+                articles.findIndex((a) => (a.articleLink = articleLink)) > -1
+              ) {
+                continue;
+              }
+              try {
+                const article = await axios.get(articleLink);
+                if (article && article.data) {
+                  const artcileString = article.data as string;
+                  const $ = cheerio.load(artcileString);
+                  $("img").remove();
+                  $("iframe").remove();
+                  let title = $("h1.title").text();
+                  console.log(title);
+                }
+              } catch (innerError: any) {
+                this.logger.error(innerError);
+              }
+            }
+          }
+        }
+      } catch (error: any) {
+        console.log(error);
+      }
+    }
     articles = articles.filter(
       (a) => isValidArticle(a) && shouldArticleBeDisplayed(a)
     );
