525c2bce32f207eef114c1087d04482e595c0b71
Baxy123 <44103797+Baxy123@users.noreply.github.com>
2022-08-14 18:18:30 +0200

    Update scrape-sportske-novosti.service.ts
    Sportske novosti fetching links for articles of top five football leauges.

--- src/modules/scrapers/services/scrape-sportske-novosti.service.ts
+++ src/modules/scrapers/services/scrape-sportske-novosti.service.ts
@@ -8,6 +8,8 @@ import {
   isValidArticle,
   shouldArticleBeDisplayed,
 } from "@resources/common/functions";
+import * as cheerio from "cheerio";
+import axios from "axios";
 import { ScraperService } from "@scrapers/services/scraper.service";
 
 @Injectable()
@@ -24,16 +26,52 @@ export class ScrapeSportskeNovostiService implements ScraperService {
   ) {
     this.type = Portals.SPORTSKE_NOVOSTI;
     this.name = getPortalName(this.type);
-    this.link = "https://www.sportske.jutarnji.hr";
+    this.link = "https://sportske.jutarnji.hr";
     this.default = getDefaultArticle(this.type, this.link, this.name);
-    this.roots = []; //Non-RSS
-  }
+    this.roots = [
+      "https://sportske.jutarnji.hr/sn/nogomet/bundesliga/",
+      "https://sportske.jutarnji.hr/sn/nogomet/serie-a/",
+      "https://sportske.jutarnji.hr/sn/nogomet/premiership/",
+      "https://sportske.jutarnji.hr/sn/nogomet/le-championnat",
+      "https://sportske.jutarnji.hr/sn/nogomet/primera",
+      "https://sportske.jutarnji.hr/sn/nogomet/reprezentacija/",
+    ]; //Non-RSS
 
+    // Pattern za linkove na stranice koje sadrze linkove na clanke  https://sportske.jutranji.hr/sn/sport/liga    ==> [https://sportske.jutarnji.hr/sn/nogomet/bundesliga/  ,https://sportske.jutarnji.hr/sn/kosarka/aba-liga, tak i za druge],
+  }
+  //Dohvation linkove na clanke
   async scrape(): Promise<Article[]> {
     let articles: Article[] = [];
-    articles = articles.filter(
+    try {
+      for (var i = 0; i < this.roots.length; i++) {
+        try {
+          let articleLinks = [];
+          let articleData = await axios.get(this.roots[i]);
+          if (articleData && articleData.data) {
+            let data = articleData.data as string;
+            let $ = cheerio.load(data);
+            $("image").remove();
+            $("iframe").remove();
+            /*   let article = $("a.card_article-link").attr("href");
+            console.log(article); */
+            let links = $("main a[class=card__article-link]").each(
+              (index, el) => {
+                let articleLink = $(el).attr("href");
+                if (articleLinks.findIndex((el) => el == articleLink) == -1) {
+                  articleLinks.push(articleLink);
+                }
+              }
+            );
+          }
+          console.log(articleLinks);
+        } catch (innerError: any) {
+          console.log(innerError);
+        }
+      }
+    } catch (error) {}
+    let $ = (articles = articles.filter(
       (a) => isValidArticle(a) && shouldArticleBeDisplayed(a)
-    );
+    ));
     this.logger.info(
       "Scraped '%d' articles from '%s'",
       articles.length,
