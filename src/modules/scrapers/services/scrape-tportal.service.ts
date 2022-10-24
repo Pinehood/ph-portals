b2f82529c885bbe13853ed638f993b93144c00e5
fiki574 <fis333@hotmail.com>
2022-10-24 19:14:46 +0200

    Last updates and additions, all working & supported

--- src/modules/scrapers/services/scrape-tportal.service.ts
+++ src/modules/scrapers/services/scrape-tportal.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectPinoLogger, PinoLogger } from "nestjs-pino";
+import axios from "@resources/common/axios";
+import * as cheerio from "cheerio";
 import { Portals } from "@resources/common/constants";
 import { Article } from "@resources/dtos";
 import {
@@ -7,6 +9,7 @@ import {
   getPortalName,
   isValidArticle,
   shouldArticleBeDisplayed,
+  TryCatch,
 } from "@resources/common/functions";
 import { ScraperService } from "@scrapers/services/scraper.service";
 
@@ -26,18 +29,101 @@ export class ScrapeTportalService implements ScraperService {
     this.name = getPortalName(this.type);
     this.link = "https://www.tportal.hr";
     this.default = getDefaultArticle(this.type, this.link, this.name);
-    this.roots = []; //Non-RSS
+    this.roots = ["https://www.tportal.hr/najnovije-vijesti"];
   }
 
   async articleLinks(): Promise<string[]> {
-    return null;
+    const articleLinks: string[] = [];
+    for (let i = 0; i < this.roots.length; i++) {
+      const rootLink = this.roots[i];
+      await TryCatch(this.logger, rootLink, async () => {
+        const articlesData = await axios.get(rootLink);
+        if (articlesData && articlesData.data) {
+          const $ = cheerio.load(articlesData.data as string);
+          $("a.articlePreview").each((_index, el) => {
+            const articleLink = $(el).attr("href");
+            if (articleLinks.findIndex((el) => el == articleLink) == -1) {
+              if (articleLink.startsWith("//")) {
+                articleLinks.push("https:" + articleLink);
+              } else if (articleLink.startsWith("/")) {
+                articleLinks.push(this.link + articleLink);
+              } else {
+                articleLinks.push(articleLink);
+              }
+            }
+          });
+        }
+      });
+    }
+    return articleLinks;
   }
 
   async scrape(): Promise<Article[]> {
     let articles: Article[] = [];
-    articles = articles.filter(
-      (a) => isValidArticle(a) && shouldArticleBeDisplayed(a)
-    );
+    const articleLinks = await this.articleLinks();
+    if (articleLinks && articleLinks.length > 0) {
+      for (let i = 0; i < articleLinks.length; i++) {
+        const articleLink = articleLinks[i];
+        if (articles.findIndex((a) => a.articleLink == articleLink) > -1)
+          continue;
+
+        await TryCatch(this.logger, articleLink, async () => {
+          const article = await axios.get(articleLink);
+          if (article && article.data) {
+            const articleHtml = article.data as string;
+            const $ = cheerio.load(articleHtml);
+            $("img").remove();
+            $("iframe").remove();
+            $("div.banner").remove();
+            $("div.embedComponent").remove();
+            $("div.autoRelatedArticles").remove();
+            $("div.listComponentType2").remove();
+            $("figure").remove();
+            $("figcaption").remove();
+            let title = $("h1.title").text();
+            if (title) {
+              title = title.replace(/\n/g, "").trim();
+            }
+            let lead = $("p.leadTitle").first().text();
+            if (lead) {
+              lead = lead.replace(/\n/g, "").trim();
+            }
+            let time = $("li.publishDate").text();
+            if (time) {
+              time = time.replace("Objavljeno", "").replace(/\n/g, "").trim();
+            }
+            let author = $("ul.meta").first().text();
+            if (author) {
+              author = author
+                .replace("Autor: ", "")
+                .replace(/\n/g, "")
+                .replace(/  /g, "")
+                .split("Zadnja")[0]
+                .trim();
+            }
+            let content = $("section.articleComponents").html();
+            if (content) {
+              content = content.replace(/\n/g, "").trim();
+            }
+            articles.push({
+              ...this.default,
+              articleId: Math.floor(
+                100000000 + Math.random() * 900000000
+              ).toString(),
+              articleLink,
+              author,
+              content,
+              lead,
+              time,
+              title,
+            });
+          }
+        });
+      }
+      articles = articles.filter(
+        (a) => isValidArticle(a) && shouldArticleBeDisplayed(a)
+      );
+    }
     this.logger.info(
       "Scraped '%d' articles from '%s'",
       articles.length,
--- src/modules/scrapers/services/scrape-vecernji.service.ts
+++ src/modules/scrapers/services/scrape-vecernji.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectPinoLogger, PinoLogger } from "nestjs-pino";
+import axios from "@resources/common/axios";
+import * as cheerio from "cheerio";
 import { Portals } from "@resources/common/constants";
 import { Article } from "@resources/dtos";
 import {
@@ -7,6 +9,7 @@ import {
   getPortalName,
   isValidArticle,
   shouldArticleBeDisplayed,
+  TryCatch,
 } from "@resources/common/functions";
 import { ScraperService } from "@scrapers/services/scraper.service";
 
@@ -26,18 +29,93 @@ export class ScrapeVecernjiService implements ScraperService {
     this.name = getPortalName(this.type);
     this.link = "https://www.vecernji.hr";
     this.default = getDefaultArticle(this.type, this.link, this.name);
-    this.roots = []; //Non-RSS
+    this.roots = [
+      "https://www.vecernji.hr/hrvatska",
+      "https://www.vecernji.hr/crna-kronika",
+      "https://www.vecernji.hr/svijet",
+      "https://www.vecernji.hr/sport",
+      "https://www.vecernji.hr/zagreb",
+      "https://www.vecernji.hr/showbiz",
+    ];
   }
 
   async articleLinks(): Promise<string[]> {
-    return null;
+    const articleLinks: string[] = [];
+    for (let i = 0; i < this.roots.length; i++) {
+      const rootLink = this.roots[i];
+      await TryCatch(this.logger, rootLink, async () => {
+        const articlesData = await axios.get(rootLink);
+        if (articlesData && articlesData.data) {
+          const $ = cheerio.load(articlesData.data as string);
+          $("a.card__link").each((_index, el) => {
+            const articleLink = $(el).attr("href");
+            if (articleLinks.findIndex((el) => el == articleLink) == -1) {
+              articleLinks.push(rootLink + articleLink);
+            }
+          });
+        }
+      });
+    }
+    return articleLinks;
   }
 
   async scrape(): Promise<Article[]> {
     let articles: Article[] = [];
-    articles = articles.filter(
-      (a) => isValidArticle(a) && shouldArticleBeDisplayed(a)
-    );
+    const articleLinks = await this.articleLinks();
+    if (articleLinks && articleLinks.length > 0) {
+      for (let i = 0; i < articleLinks.length; i++) {
+        const articleLink = articleLinks[i];
+        if (articles.findIndex((a) => a.articleLink == articleLink) > -1)
+          continue;
+
+        await TryCatch(this.logger, articleLink, async () => {
+          const article = await axios.get(articleLink);
+          if (article && article.data) {
+            const articleHtml = article.data as string;
+            const $ = cheerio.load(articleHtml);
+            $("img").remove();
+            $("iframe").remove();
+            $("span.widgetWrap").remove();
+            $("div.article__body_banner_article_bottom").remove();
+            let title = $("h1.article__title").text();
+            if (title) {
+              title = title.replace(/\n/g, "").trim();
+            }
+            let lead = $("span.article__lead").text();
+            if (lead) {
+              lead = lead.replace(/\n/g, "").trim();
+            }
+            let time = $("span.article__header_date").text();
+            if (time) {
+              time = time.replace(/\n/g, "").trim();
+            }
+            let author = $("a.article__author--link").text();
+            if (author) {
+              author = author.replace(/\n/g, "").replace(/  /g, "").trim();
+            }
+            let content = $("div.article__body--main_content").html();
+            if (content) {
+              content = content.replace(/\n/g, "").trim();
+            }
+            articles.push({
+              ...this.default,
+              articleId: articleLink.substring(
+                articleLink.lastIndexOf("-") + 1
+              ),
+              articleLink,
+              author,
+              content,
+              lead,
+              time,
+              title,
+            });
+          }
+        });
+      }
+      articles = articles.filter(
+        (a) => isValidArticle(a) && shouldArticleBeDisplayed(a)
+      );
+    }
     this.logger.info(
       "Scraped '%d' articles from '%s'",
       articles.length,
