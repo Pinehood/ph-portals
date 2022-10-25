074f9b66f410c6a21b10d6917fbdb4d37de3e1b2
fiki574 <fis333@hotmail.com>
2022-10-25 09:12:30 +0200

    Remove UTC dates, missing space updates, package upgrade

--- src/common/functions.ts
+++ src/common/functions.ts
@@ -126,17 +126,17 @@ export function redirect(portal: Portals): string {
 
 export function formatDate(date: Date): string {
   return (
-    ("0" + date.getUTCDate()).slice(-2) +
+    ("0" + date.getDate()).slice(-2) +
     "." +
-    ("0" + (date.getUTCMonth() + 1)).slice(-2) +
+    ("0" + (date.getMonth() + 1)).slice(-2) +
     "." +
-    date.getUTCFullYear() +
-    "." +
-    ("0" + date.getUTCHours()).slice(-2) +
+    date.getFullYear() +
+    ". " +
+    ("0" + date.getHours()).slice(-2) +
     ":" +
-    ("0" + date.getUTCMinutes()).slice(-2) +
+    ("0" + date.getMinutes()).slice(-2) +
     ":" +
-    ("0" + date.getUTCSeconds()).slice(-2)
+    ("0" + date.getSeconds()).slice(-2)
   );
 }
 
