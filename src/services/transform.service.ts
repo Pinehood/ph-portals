87476ed8758414924586d1b1bceab48fe0def49f
fiki574 <fiki8796@hotmail.com>
2023-09-05 16:01:21 +0200

    chore(deps): package version upgrades

--- src/services/transform.service.ts
+++ src/services/transform.service.ts
@@ -3,12 +3,16 @@ import { Article } from "@/dtos";
 
 export class TransformService {
   static article(article: Article, config: ScraperConfig): void {
-    article.title = TransformService.single(article.title, config.title);
-    article.lead = TransformService.single(article.lead, config.lead);
-    article.time = TransformService.single(article.time, config.time);
-    article.author = TransformService.single(article.author, config.author);
-    if (article.content) {
-      article.content = article.content.replace(/\n/g, "");
+    try {
+      article.title = TransformService.single(article.title, config.title);
+      article.lead = TransformService.single(article.lead, config.lead);
+      article.time = TransformService.single(article.time, config.time);
+      article.author = TransformService.single(article.author, config.author);
+      if (article.content) {
+        article.content = article.content.replace(/\n/g, "");
+      }
+    } catch {
+      return;
     }
   }
 
