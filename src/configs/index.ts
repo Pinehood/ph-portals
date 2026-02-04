bfb2c68faf047cedc048e1001b8e2ddf6b9d9787
fiki574 <bruno.fistrek@growit.hr>
2026-02-04 18:27:09 +0100

    fix(core): imports

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,7 +1,6 @@
 export * from "./home.config";
 export * from "./scrape-24sata.config";
 export * from "./scrape-direktno.config";
-export * from "./scrape-dnevnik.config";
 export * from "./scrape-dnevno.config";
 export * from "./scrape-index.config";
 export * from "./scrape-jutarnji.config";
@@ -9,7 +8,6 @@ export * from "./scrape-n1info.config";
 export * from "./scrape-net.config";
 export * from "./scrape-novi-list.config";
 export * from "./scrape-poslovni.config";
-export * from "./scrape-slobodna-dalmacija.config";
 export * from "./scrape-sportske-novosti.config";
 export * from "./scrape-tportal.config";
 export * from "./scrape-vecernji.config";
