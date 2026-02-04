df92d577e952147cf70af34013b84f8c6f099782
fiki574 <bruno.fistrek@growit.hr>
2026-02-04 18:17:52 +0100

    feat(core): try re-enable prod swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { setupApp, setupSwagger, start } from "./setup";
 
 (async function entrypoint() {
   const app = await setupApp();
-  // await setupSwagger(app);
+  await setupSwagger(app);
   await start(app);
 })();
